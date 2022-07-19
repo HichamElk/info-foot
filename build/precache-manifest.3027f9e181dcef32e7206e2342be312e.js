ce49ed180d7ebcf51727bf88b8dd224747d11ecb
Hicham <helkadaoui@corefortech.com>
2022-07-20 01:24:34 +0200

    add build folder

---
+++ build/precache-manifest.3027f9e181dcef32e7206e2342be312e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a447e0262f03168df83",
+    "url": "/static/js/main.2f500a6e.chunk.js"
+  },
+  {
+    "revision": "a9f5aa1c860733ab9ad4",
+    "url": "/static/js/runtime~main.f7637b3e.js"
+  },
+  {
+    "revision": "9a5b559be1a6fe35f505",
+    "url": "/static/js/2.5cc80a3f.chunk.js"
+  },
+  {
+    "revision": "27a4875cb2d8a70e95eb",
+    "url": "/static/js/3.fd51dd62.chunk.js"
+  },
+  {
+    "revision": "8449a33e611051c6fb2c5fc25ca3e099",
+    "url": "/index.html"
+  }
+];
